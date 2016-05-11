253d974e43978cf45888b7080e67196b95ed3103
Eric Modzelewski <em3149@nyu.edu>
2016-05-11 14:38:24 -0400

    no message

---
+++ script.js
@@ -0,0 +1,9 @@
+
+
+function createShirt() {
+	var myDiv = document.createElement('div')
+	myDiv.innerHTML = 'T-shirt'
+
+	// add the div to the document body
+	document.body.appendChild('myDiv')
+}
